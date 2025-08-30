b50c974cb702725c0745d432744cd61600853e8b
Alexey_Tsibikin <alex999333444@gmail.com>
2025-08-30 11:39:20 +0400

    init

---
+++ modules/common/src/main/java/net/oxcodsnet/beltborne_lanterns/ExampleMod.java
@@ -0,0 +1,9 @@
+package net.oxcodsnet.beltborne_lanterns;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "beltborne_lanterns";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ modules/neoforge/src/main/java/net/oxcodsnet/beltborne_lanterns/neoforge/ExampleModNeoForge.java
@@ -0,0 +1,13 @@
+package net.oxcodsnet.beltborne_lanterns.neoforge;
+
+import net.neoforged.fml.common.Mod;
+
+import net.oxcodsnet.beltborne_lanterns.ExampleMod;
+
+@Mod(ExampleMod.MOD_ID)
+public final class ExampleModNeoForge {
+    public ExampleModNeoForge() {
+        // Run our common setup.
+        ExampleMod.init();
+    }
+}
