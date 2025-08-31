a9a84760b7e9fdb1bf7b4af663bd3a9a7cfa20b8
Alexey_Tsibikin <alex999333444@gmail.com>
2025-08-31 20:15:58 +0400

    correct

--- modules/common/src/main/java/net/oxcodsnet/beltborne_lanterns/BLMod.java
+++ modules/common/src/main/java/net/oxcodsnet/beltborne_lanterns/BLMod.java
@@ -7,14 +7,5 @@ public final class BLMod {
 
     public static void init() {
         LampRegistry.init();
-
-        // TODO: Проведи рефакторинг LambDynLightsCompat:
-        //      1. Проверь на правильность
-        //      2. Константы и прочее, вынести в конфиг
-        // TODO: Когда игрок умирает:
-        //      При gamerule keepInventory false - сделать так, чтобы лампа выпадала
-        //      При gamerule keepInventory true - сделать так, чтобы лампа не выпадала
-        // TODO: Добавить перевод конфиг файла для всех языков в BLLanguage
-
     }
 }
